efb89420d1fb2d93d6d244e47914b8bb1e458d07
Colin Wong <cwongmath@gmail.com>
2021-01-26 19:12:11 -0600

    Update currency sizings

--- Assets/Scripts/CurrencySystem.cs
+++ Assets/Scripts/CurrencySystem.cs
@@ -1,19 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CurrencySystem : MonoBehaviour
 {
+    [SerializeField]
+    private int startingBalance = 100;
+
     public int balance;
+
+    private Text CurrencyDisplay;
+
+    void Awake()
+    {
+        CurrencyDisplay = GameObject.Find("BalanceCounter").GetComponent<Text>();
+    }
 
     void Start()
     {
-        balance = 0;
+        balance = startingBalance;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        CurrencyDisplay.text = balance.ToString();
     }
+
+
 }
